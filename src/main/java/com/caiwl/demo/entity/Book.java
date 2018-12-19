4cf204ef65e959c36e1b233245e37412f1743d38
caiwl <caiwenlong@aloestec.com>
2018-12-19 09:08:35 +0800

    initial

--- src/main/java/com/caiwl/demo/entity/Book.java
+++ src/main/java/com/caiwl/demo/entity/Book.java
@@ -1,11 +1,14 @@
 package com.caiwl.demo.entity;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 
 @Entity
+@Data
 public class Book {
     @Id
     @GeneratedValue
